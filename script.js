76dabe78a13e4ee16146bb317cfb6c4cdd00d78d
FrederickJordan <17fjsetiawan@gmail.com>
2026-05-12 03:43:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your Firebase config – replace with your own
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA-aOYS4SxBin4ks17MzX_TVtnxzjPLhD8",
+  authDomain: "quizguard-d8c56.firebaseapp.com",
+  databaseURL: "https://quizguard-d8c56-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizguard-d8c56",
+  storageBucket: "quizguard-d8c56.firebasestorage.app",
+  messagingSenderId: "1025907204447",
+  appId: "1:1025907204447:web:047dc148e89e49e21456ea",
+  measurementId: "G-YBKTDS05H3"
 };
 
 // Initialize
