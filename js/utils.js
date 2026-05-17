35e14d903c66e91efb460c2c0a98b3a49abff465
Plsssssssss <weih3107@gmail.com>
2026-05-17 09:11:52 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -7,6 +7,16 @@ export function escapeHtml(str) {
   return str.replace(/[&<>]/g, m => ({ '&':'&amp;', '<':'&lt;', '>':'&gt;' }[m]));
 }
 
+// Fisher-Yates shuffle algorithm
+export function shuffleArray(array) {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 export function addLog(message) {
   const logArea = getEl("logArea");
   if (!logArea) return;
