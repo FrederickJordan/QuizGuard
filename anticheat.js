e94172b8c05b671eea3a645e8b16e7630b69049b
Plsssssssss <weih3107@gmail.com>
2026-05-22 13:47:32 +0800

    Update anticheat.js

--- anticheat.js
+++ anticheat.js
@@ -35,6 +35,7 @@ export function showPauseOverlay(message, showContinueButton = false) {
   title.textContent = showContinueButton ? 'Warning!' : 'Quiz Paused';
   msg.textContent = message;
   continueBtn.style.display = showContinueButton ? 'inline-flex' : 'none';
+  overlay.style.background = showContinueButton ? '#000000' : 'rgba(0, 0, 0, 0.9)';
   overlay.style.display = 'flex';
 }
 
